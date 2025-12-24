77ec14166eacbf16a6cfaa4df4217c38d9489fb4
Arpit Singh <arpitlibono57@gmail.com>
2025-12-24 21:41:40 +0530

    v-3 props shifting

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,146 +1,207 @@
-
 <template>
 
-<div id="Registration">
-    <div id="RegistrationBox" >
-     <div id="box1">
-       
-        <form @submit.prevent="FormSubmit" > 
-           
-            <div id="Fields">
+    <div id="Registration">
+        <div id="RegistrationBox">
+            <div id="box1">
 
-            <div> 
-                <label for="name" title="Name" id="Fields" >Name:</label>
-             
+                <form @submit.prevent="functionAct ? editFormSubmit(editIdx) : FormSubmit()">
+
+                    <div id="Fields">
+
+                        <div>
+                            <label for="name" title="Name" id="Fields">Name:</label>
+
+                        </div>
+                        <div>
+                            <label for="email" title="Email" id="Fields">Email</label>
+
+                        </div>
+                        <div>
+                            <label for="reg" title="Reg" id="Fields">Reg:</label>
+
+                        </div>
+                        <div>
+                            <label for="num" title="Phone" id="Fields">Phone</label>
+
+                        </div>
+                        <div>
+                            <label for="Gender" title="Gender" id="Fields">Gender:</label>
+                        </div>
+                        <div>
+                            <label for="message" title="message" id="Fields">message</label>
+                        </div>
+                    </div>
+
+                    <div id="FieldsInput">
+                        <input placeholder="Enter your name " name="Name" type="text" id="FieldsInput" v-model="name">
+                        </input>
+                        <input placeholder="Enter your email " name="Email" type="email" id="FieldsInput"
+                            v-model="email"> </input>
+                        <input placeholder="Enter your reg " name="Reg" type="text" id="FieldsInput" v-model="reg">
+                        </input>
+                        <input placeholder="Enter your phone number " name="Phone" type="tel" id="FieldsInput"
+                            v-model="phone"> </input>
+                        <input placeholder="Enter your gender" name="Gender" type="text" id="FieldsInput"
+                            v-model="gender"> </input>
+                        <input placeholder="Enter your message" name="Message" type="text" id="FieldsInput"
+                            v-model="message"> </input>
+                        <div>
+                            <button type="submit" id="SubmitButton">Submit</button>
+                        </div>
+                    </div>
+
+                </form>
+
+
             </div>
-            <div>
-                <label for="email" title="Email" id="Fields">Email</label>
-                 
-            </div>
-             <div> 
-                <label for="reg" title="Reg" id="Fields" >Reg:</label>
-              
-            </div>
-            <div>
-                <label for="num" title="Phone" id="Fields">Phone</label>
-                 
-            </div>
-             <div> 
-                <label for="Gender" title="Gender" id="Fields" >Gender:</label>
-            </div>
-            <div>
-                <label for="message" title="message" id="Fields">message</label>
-            </div>
+            <div id="box2">
+                <div>
+                    {{ SubmissionStatus }}
+                </div>
+
+                <table id="Table">
+                    <thead>
+                        <tr>
+                            <th class="entry">Name</th>
+                            <th class="entry">Email</th>
+                            <th class="entry">Reg</th>
+                            <th class="entry">Phone</th>
+                            <th class="entry">Gender</th>
+                            <th class="entry">Message</th>
+                        </tr>
+                    </thead>
+                    <tbody id="TableEntry">
+
+                    </tbody>
+                </table>
+
             </div>
 
-            <div id="FieldsInput" >
-               <input placeholder="Enter your name " name="Name" type="text" id="FieldsInput" v-model="name"  > </input>
-               <input placeholder="Enter your email " name="Email" type="email" id="FieldsInput" v-model="email" > </input>
-               <input placeholder="Enter your reg " name="Reg" type="text" id="FieldsInput" v-model="reg" > </input>
-               <input placeholder="Enter your phone number " name="Phone" type="tel" id ="FieldsInput"  v-model="phone" > </input>
-               <input placeholder="Enter your gender" name="Gender" type="text" id ="FieldsInput" v-model="gender"> </input> 
-               <input placeholder="Enter your message" name="Message" type="text" id ="FieldsInput"  v-model="message"> </input>
-               <div>
-               <button type="submit" id="SubmitButton" >Submit</button>
-               </div>
-            </div>
 
-        </form>
-    
-    
-     </div> 
-     <div id="box2" >
-        <div>
-            {{ SubmissionStatus }}
         </div>
-
-        <table id="Table" >
-            <thead>
-                <tr>
-                    <th class="entry"  >Name</th>
-                    <th class="entry">Email</th>
-                    <th class="entry" >Reg</th>
-                    <th class="entry" >Phone</th>
-                    <th class="entry" >Gender</th>
-                    <th class="entry">Message</th>
-                </tr>
-            </thead>
-            <tbody id="TableEntry">
-                
-            </tbody>
-        </table>
-
-     </div>
 
 
     </div>
-
-
-</div>
+    <RegistrationPage2 :DataEntry=registeredUsersBox  :gettingData="getData" />
 
 </template>
 
 <script setup>
-    import { ref } from 'vue';
-    import { inject } from 'vue';
-    const SubmissionStatus=ref("");
-    const name=ref("");
-    const email=ref("");
-    const reg=ref("");  
-    const phone=ref("");
-    const gender=ref("");
-    const message=ref("");
-    const registeredUsers=inject('registeredUsers') ;
-    const FormSubmit=(e)=>{
-        e.preventDefault();
-        console.log("Form submitted");
- SubmissionStatus.value = "Form submitted successfully!";
- 
-   
+import { ref } from 'vue';
+import { inject } from 'vue';
+import RegistrationPage2 from './RegistrationPage2.vue';
+const SubmissionStatus = ref("");
+const name = ref("");
+const email = ref("");
+const reg = ref("");
+const phone = ref("");
+const gender = ref("");
+const message = ref("");
+const functionAct = ref(false) ; 
+const registeredUsersBox = ref([]);
+const editIdx = ref(-1) ; 
+const registeredUsers = inject('registeredUsers');
 
 
-   let elem=document.createElement("tr"); 
-   elem.innerHTML=`<td>${name.value}</td>
+const getData = (data)=>{
+    console.log("data recieved from child " , data )
+    functionAct.value = true ; 
+    editIdx.value = data.index ;
+    EditHandler(data.index , data.Val) ; 
+}
+
+const EditHandler = (index , Val)=>{    
+        name.value = registeredUsersBox.value[index].name ,
+        email.value = registeredUsersBox.value[index].email , 
+        reg.value = registeredUsersBox.value[index].reg , 
+        phone.value =registeredUsersBox.value[index].phone , 
+        gender.value = registeredUsersBox.value[index].gender , 
+        message.value = registeredUsersBox.value[index].message     
+}
+
+const editFormSubmit = (idx)=>{
+registeredUsersBox.value[idx] =  {
+            name: name.value,
+            email: email.value,
+            reg: reg.value,
+            phone: phone.value,
+            gender: gender.value,
+            message: message.value
+        } 
+
+         SubmissionStatus.value = "Form Edit successfully!";
+         functionAct.value = false;
+  editIdx.value = -1;
+    name.value = "";
+    email.value = "";
+    reg.value = "";
+    phone.value = "";
+    gender.value = "";
+    message.value = "";
+
+}
+
+
+
+const FormSubmit = (e) => {
+  
+    console.log("Form submitted");
+    SubmissionStatus.value = "Form submitted successfully!";
+
+
+
+
+    let elem = document.createElement("tr");
+    elem.innerHTML = `<td>${name.value}</td>
                     <td>${email.value}</td>
                     <td>${reg.value}</td>
                     <td>${phone.value}</td>
                     <td>${gender.value}</td>
                     <td>${message.value}</td>`;
-        registeredUsers.value.push(
+               
+    registeredUsers.value.push(
         {
-              name: name.value,
-  email: email.value,
-  reg: reg.value,
-  phone: phone.value,
-  gender: gender.value,
-  message: message.value
+            name: name.value,
+            email: email.value,
+            reg: reg.value,
+            phone: phone.value,
+            gender: gender.value,
+            message: message.value
         }
-  );
+    );
+
+    registeredUsersBox.value.push({
+        name: name.value,
+        email: email.value,
+        reg: reg.value,
+        phone: phone.value,
+        gender: gender.value,
+        message: message.value
+    })
+
 
     document.getElementById("TableEntry").appendChild(elem);
-    for(let i = 0 ; i <store.entries.length ; i++){
-        console.log(store.entries[i]) ;
-    }
+   
 
-     name.value = "";
-  email.value = "";
-  reg.value = "";
-  phone.value = "";
-  gender.value = "";
-  message.value = "";
-    }
+    name.value = "";
+    email.value = "";
+    reg.value = "";
+    phone.value = "";
+    gender.value = "";
+    message.value = "";
+}
 
 </script>
 
 <style scoped>
-
-.entry{
+.entry {
     padding: 10px;
     border: 1px solid gray;
     text-align: center;
     color: white;
 }
-#SubmitButton{
+
+#SubmitButton {
     margin-top: 1rem;
     padding: 0.5rem 1rem;
     font-size: 1rem;
@@ -152,7 +213,8 @@
     background-color: rgb(231, 61, 61);
     width: 50%;
 }
-#Registration{
+
+#Registration {
     box-sizing: border-box;
     margin: 0;
     padding: 1%;
@@ -166,13 +228,13 @@
 }
 
 
-#RegistrationBox{
+#RegistrationBox {
     width: 100%;
     height: 100%;
     margin: auto;
-  
+
     display: flex;
-  
+
     justify-content: space-between;
     align-items: center;
     gap: 10px;
@@ -182,18 +244,19 @@
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-#box1,#box2{
+#box1,
+#box2 {
     width: 45%;
     height: 80%;
     padding: 2rem;
-  
-  
-    display: flex ;
+
+
+    display: flex;
     justify-content: start;
     align-items: center;
 }
 
-#Fields{
+#Fields {
     padding: 0.5rem;
     font-size: 1.2rem;
     font-weight: normal;
@@ -201,21 +264,24 @@
     flex-direction: column;
 
 }
-#FieldsInput{
+
+#FieldsInput {
     margin: 0.2rem;
     padding: 0.5rem;
     font-size: 1rem;
     font-weight: normal;
     border-radius: 2rem;
 }
-#box2{
+
+#box2 {
     font-size: 2rem;
     font-weight: bold;
     color: yellowgreen;
     display: flex;
     flex-direction: column;
 }
-#box1 form{
+
+#box1 form {
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -223,7 +289,8 @@
 }
 
 
-#Table,#TableEntry{
+#Table,
+#TableEntry {
     margin-top: 10px;
     font-size: 1rem;
     border-collapse: collapse;
@@ -231,7 +298,8 @@
     height: auto;
     width: 100%;
 }
-#TableEntry{
+
+#TableEntry {
     text-align: center;
 }
 </style>
---
+++ src/components/RegistrationPage2.vue
@@ -0,0 +1,126 @@
+
+
+<template>
+
+<div id="mainbody" >
+
+    <div id="TableData">
+        
+        <Table>
+            <thead>
+                <tr>
+                   <th class="entry"  >Name</th>
+                    <th class="entry">Email</th>
+                    <th class="entry" >Reg</th>
+                    <th class="entry" >Phone</th>
+                    <th class="entry" >Gender</th>
+                    <th class="entry">Message</th>
+                </tr>
+                
+            </thead>
+            
+            <tbody>
+                <tr @dblclick="EditEntry(index)"  v-for="(item, index) in DataEntry" :key="index">
+                   <td>{{ item.name}}</td>
+                    <td>{{  item.email}}</td>
+                     <td>{{ item.reg }}</td>
+                      <td>{{item.phone  }}</td>
+                       <td>{{ item.gender}}</td>
+                        <td>{{  item.message}}</td> 
+                </tr>
+            </tbody>
+        </Table>
+
+
+    </div>
+
+</div>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+    DataEntry : Array , 
+    gettingData : Function , 
+})
+
+const EditEntry = (index)=>{
+  const value = props.DataEntry[index] ; 
+  const data  = {index : index , Val : value }  ; 
+  props.gettingData(data)
+}
+
+
+</script>
+
+<style scoped >
+#mainbody{
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    background-color: rgb(32, 31, 31);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#TableData{
+    border: 2px solid white;
+    height: 70vh;
+    width: 90vw;
+}
+table{
+    border-collapse: collapse;
+    padding: 20px;
+}
+tr{
+    padding: 0.5rem;
+    width: 100%;
+
+}
+th{
+    
+    width: 2%;
+    text-align: center;
+    padding: 10px;
+    color: orangered;
+   
+}
+td{
+    width: 2%;
+    text-align: center;
+    padding: 10px;
+    cursor: pointer;
+  
+}
+/* td::before{
+       width: 2%;
+    text-align: center;
+    padding: 10px;
+    cursor: pointer;
+    transition: 2s;
+}
+td::after{
+color:beige ;
+font-size: 1rem;
+} */
+tr:hover{
+    color: orchid;
+    font-size: 1rem ;
+    font-weight: 900;
+
+}
+thead{
+    border-bottom: 1px solid white  ;
+}
+
+#line{
+    width: 100%;
+}
+
+
+
+</style>
